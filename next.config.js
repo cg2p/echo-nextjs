1376905853422b0467fad2185ed1a5709e7bd4bc
Jeremy Caine <jezcaine@gmail.com>
2020-04-19 19:57:37 +1000

    add concurrently

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 // next.config.js
 require('dotenv').config();
 
+const env = process.env.NODE_ENV; // 'dev' or 'test'
+
 module.exports = {
     serverRuntimeConfig: { // Will only be available on the server side
-        mySecret: 'secret'
+        mySecret: 'secret',
+        version: process.env.VERSION,
+        env: process.env.NODE_ENV
     },
 
     publicRuntimeConfig: { // Will be available on both server and client
         staticFolder: '/static',
-        version: process.env.VERSION
+        version: process.env.VERSION,
+        env: process.env.NODE_ENV
     }
 };
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,13 @@ import Layout from '../components/Layout.js';
 import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig();
-const { version } = publicRuntimeConfig;
+const { env, version } = publicRuntimeConfig;
 
 const indexPageContent = <p>Hello Next.js</p>;
 
 export default function Index() {
   console.log("build version = %s", version);
+  console.log("env = %s", env);
   return <Layout content={indexPageContent} />;
 }
 
